2cc9afee4e9756ab0dabc5fc73000db5fb80d9b8
Mygapula Srujan Prabhu Kumar <srujanprabhu18@gmail.com>
2024-01-05 21:08:30 +0530

    Add files via upload

---
+++ snakeproject/snake.py
@@ -0,0 +1,57 @@
+from turtle import Turtle, Screen
+
+STARTING_POS = [(0, 0), (-20,0), (-40,0)]
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POS:
+            self.add_segment(position)
+
+    def add_segment(self,position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def up(self):
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+    def move(self):
+         for seg_num in range(len(self.segments) - 1 ,0,-1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x,new_y)
+
+
+         self.segments[0].forward(20)
+
+
+
+
+
+
